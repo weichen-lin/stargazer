64d93e5b826b00365e6e016ac07be2825455235c
WeiChen Lin <asdfg09487@gmail.com>
2024-05-07 23:47:15 +0800

    :sparkles: Add some backdrop

--- next-app/app/stars/page.tsx
+++ next-app/app/stars/page.tsx
@@ -10,9 +10,10 @@ export default async function Dashboard() {
       className={clsx(
         'bg-blue-50/70 dark:bg-slate-900 flex flex-col gap-3 items-center',
         'w-full md:flex-row md:flex-wrap md:items-center md:justify-center lg:p-12 lg:gap-6',
+        'from-blue-100/40 to-blue-200/60 bg-gradient-to-br dark:from-slate-500 dark:to-slate-100',
       )}
     >
-      <div className='flex flex-col w-[380px] h-[380px] justify-between items-center'>
+      <div className='flex flex-col w-[380px] h-[380px] justify-between items-center backdrop-blur-md backdrop-opacity-60 opacity-75'>
         <TotalRepositories />
         <Crontab />
       </div>

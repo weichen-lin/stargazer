cb641d7ad109949641bdd3f4d0507b1bf385b574
WeiChen Lin <asdfg09487@gmail.com>
2024-06-25 16:12:35 +0800

    :zap: Change healthy check method

--- kafka-service/main.go
+++ kafka-service/main.go
@@ -32,7 +32,7 @@ func main() {
 
 	r := gin.Default()
 
-	r.GET("/health", func(c *gin.Context) {
+	r.HEAD("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "OK",
 		})

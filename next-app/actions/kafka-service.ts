db9a4947ed6f64f3fcbb195f32b6b19d1a541ec9
WeiChen Lin <asdfg09487@gmail.com>
2024-07-07 14:45:43 +0800

    :sparkles: Switch to kafka service

--- next-app/actions/kafka-service.ts
+++ next-app/actions/kafka-service.ts
@@ -5,7 +5,7 @@ const PRODUCER_URL = process.env.PRODUCER_URL
 
 export async function syncUserStars(email: string): Promise<{ status: number; title: string; message: string }> {
   const jwtToken = await generateAccessToken(email)
-  const response = await fetch(`http://localhost:8080/get_user_stars`, {
+  const response = await fetch(`${process.env.KAFKA_SERVICE}/get_user_stars`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -39,7 +39,7 @@ export async function syncUserStars(email: string): Promise<{ status: number; ti
 
 export async function updateCrontabHour(email: string, hour: number): Promise<boolean> {
   const jwtToken = await generateAccessToken(email)
-  const response = await fetch(`${PRODUCER_URL}/update_cron_tab_setting?hour=${hour}`, {
+  const response = await fetch(`${process.env.KAFKA_SERVICE}/update_cron_tab_setting?hour=${hour}`, {
     method: 'PATCH',
     headers: {
       'Content-Type': 'application/json',
--- next-app/app/dashboard/components/total.tsx
+++ next-app/app/dashboard/components/total.tsx
@@ -26,7 +26,7 @@ export default function TotalRepositories() {
   return (
     <div
       className={clsx(
-        'border-[2px] border-slate-300/70 rounded-lg flex flex-col items-center justify-center gap-y-4',
+        'border-[2px] border-slate-300/70 rounded-lg flex flex-col items-center justify-center gap-y-1',
         'bg-white drop-shadow-lg dark:bg-slate-300 dark:border-slate-800 dark:text-white',
         'w-full h-[35%]',
       )}

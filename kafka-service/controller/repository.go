b7b016122034d33c8d56304fb2aeda4ec91fb8d3
WeiChen Lin <asdfg09487@gmail.com>
2024-08-18 22:59:06 +0800

    :sparkles: Add get error handle

--- kafka-service/controller/repository.go
+++ kafka-service/controller/repository.go
@@ -1,23 +1,40 @@
 package controller
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/weichen-lin/kafka-service/db"
 	"github.com/weichen-lin/kafka-service/domain"
 )
 
-
-
 func (c *Controller) GetRepository(ctx *gin.Context) {
-	repo, err := c.db.GetRepository(ctx, 123456)
-
+	repo_idStr := ctx.Param("repo_id")
+	repo_id, err := strconv.ParseInt(repo_idStr, 10, 64)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error() })
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid repo_id"})
 		return
 	}
 
-	ctx.JSON(http.StatusOK, repo.ToRepositoryEntity())
+	repo, err := c.db.GetRepository(ctx, repo_id)
+
+	switch {
+	case err == nil:
+		ctx.JSON(http.StatusOK, repo.ToRepositoryEntity())
+		return
+    case errors.Is(err, db.ErrRepositoryNotFound):
+        ctx.JSON(http.StatusNotFound, gin.H{"error": fmt.Sprintf("repository %d not found", repo_id)})
+		return
+    case errors.Is(err, db.ErrNotFoundEmailAtContext):
+        ctx.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		return
+    default:
+        ctx.JSON(http.StatusForbidden, gin.H{"error": ""})
+		return
+    }
 }
 
 func (c *Controller) CreateRepository(ctx *gin.Context) {
